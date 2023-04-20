706a30cedcc64acad4348e1cffbbcaafa4559607
yakkub <yalkabsh@sfsu.edu>
2023-04-19 23:11:31 -0700

    Milestone 2 finished, added all hbs files/partails

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Yakoub Alkabsh" });
 });
 
+router.get('/index', function(req,res){
+  res.render('index', {js:["index.js"]});
+
+});
+
+router.get('/login', function(req,res){
+  res.render('login');
+});
+
+router.get('/profile', function(req,res){
+  res.render('profile');
+});
+
+router.get('/postvideo', function(req,res){
+  res.render('postvideo');
+});
+
+router.get('/registration', function(req,res){
+  res.render('registration',{js:["registration.js"]});
+});
+
+router.get('/viewpost/:id(\\d+)', function(req,res){
+  res.render('viewpost');
+});
+
 module.exports = router;
