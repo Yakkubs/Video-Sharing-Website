ca1f5b042775cf3589b319625906674c10cbe6a2
Yakkubs <Yakoub12345@gmail.com>
2023-05-16 18:36:50 -0700

    added css to flash errors

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -1,27 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var multer = require('multer');
+var multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "public/videos/uploads")
-        
+        cb(null, "public/videos/uploads");
     },
     filename: function (req, file, cb) {
         var fileExt = file.mimetype.split("/")[1];
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, `${file.fieldname}-${uniqueSuffix}.${fileExt}
-        `);
+        cb(null, `${file.fieldname}-${uniqueSuffix}.${fileExt}`);
     },
 });
 
 const upload = multer({ storage: storage });
 
-router.post("/create", upload.single("video") ,function (req, res, next) {
+router.post("/create", upload.single("video"), function (req, res, next) {
     console.log(req.file);
     console.log(req.body);
     res.end();
 });
-
 
 module.exports = router;
